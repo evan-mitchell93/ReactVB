2b746a1c95ca66249bbab7fd6d9323e5b0b2d3cd
evan-mitchell93 <emitchell41@ivytech.edu>
2024-07-19 09:48:38 -0400

    resultcard styling
    Started shaping the result card. Scores are shown under each team abbr. and rounded the corners.

--- SOCS_VB/components/ResultCard.js
+++ SOCS_VB/components/ResultCard.js
@@ -3,10 +3,14 @@ import {React,Text,View,Image,StyleSheet} from "react-native";
 export default function ResultCard(props){
     return(
         <View style={resultStyles.content}>
-            <Text>SOCS</Text>
-            <Text>{props.res.opponent}</Text>
-            <Text>{props.res.setsWon}</Text>
-            <Text>{props.res.setsLost}</Text>
+            <View style={resultStyles.team_box}>
+                <Text>SOCS</Text>
+                <Text>  {props.res.setsWon}  </Text>
+            </View>
+            <View style={resultStyles.team_box}>
+                <Text>{props.res.opponent}</Text>
+                <Text>  {props.res.setsLost}  </Text>
+            </View>
         </View>
     );
 }
@@ -17,9 +21,23 @@ const resultStyles = StyleSheet.create({
         flexDirection: 'row',
         backgroundColor: '#fff',
         padding: "10px",
-        shadowColor: '#000',
-        shadowRadius: 5,
-        marginTop: '10px',
-        maxHeight: '100px',
+        marginTop: 20,
+        marginBottom: 10,
+        maxHeight: 100,
+        maxWidth: '50%',
+        shadowColor: 'black',
+        shadowOpacity: 1,
+        elevation: 3,
+        zIndex: 999,
+        borderRadius: 10,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+
+    team_box: {
+        flex: 1, 
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
     }
 })
